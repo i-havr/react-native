4a366942b51dd1a84e3b2fb861867e29ba2bd76c
i-havr <i.havrylov@gmail.com>
2023-05-15 14:20:29 +0300

    add register and login pages

---
+++ src/Screens/LoginScreen.jsx
@@ -0,0 +1,229 @@
+import { useState } from "react";
+
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+import bgImage from "../../assets/images/bg.jpg";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+export default function LoginScreen() {
+  const [formData, setFormData] = useState(initialState);
+  const [isHidePassword, setIsHidePassword] = useState(true);
+  // const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { email, password } = formData;
+
+    if (email.length === 0 || password.length === 0) {
+      console.log("All fields are required!");
+      return;
+    }
+
+    const normalizedFormData = {
+      email: email.toLowerCase(),
+      password,
+    };
+
+    Keyboard.dismiss();
+    setFormData(initialState);
+    console.log(normalizedFormData, "Login data has been sent");
+  };
+
+  // const keyboardDidHideCallback = () => {
+  //   setIsShowKeyboard(false);
+  // };
+
+  const keyboardHide = () => {
+    // setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
+
+  // useEffect(() => {
+  //   const keyboardDidHide = Keyboard.addListener(
+  //     "keyboardDidHide",
+  //     keyboardDidHideCallback
+  //   );
+
+  //   return () => {
+  //     keyboardDidHide?.remove();
+  //   };
+  // });
+
+  return (
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={keyboardHide}>
+        <ImageBackground source={bgImage} style={styles.bgImage}>
+          <TouchableWithoutFeedback onPress={keyboardHide}>
+            <View style={styles.formWrapper}>
+              <Text style={styles.text}>Login</Text>
+              <KeyboardAvoidingView
+                behavior={Platform.OS == "ios" ? "padding" : "height"}
+              >
+                <View style={{ marginBottom: 16 }}>
+                  <TextInput
+                    style={styles.input}
+                    onChangeText={(value) =>
+                      setFormData((prevState) => ({
+                        ...prevState,
+                        email: value,
+                      }))
+                    }
+                    value={formData.email}
+                    placeholder="Email"
+                    keyboardType="email-address"
+                    placeholderTextColor="#BDBDBD"
+                    // onFocus={() => {
+                    //   setIsShowKeyboard(true);
+                    // }}
+                  />
+                </View>
+                <View style={{ position: "relative" }}>
+                  <TextInput
+                    style={{
+                      ...styles.input,
+                      paddingRight: 65,
+                    }}
+                    onChangeText={(value) =>
+                      setFormData((prevState) => ({
+                        ...prevState,
+                        password: value,
+                      }))
+                    }
+                    value={formData.password}
+                    placeholder="Password"
+                    secureTextEntry
+                    keyboardType="default"
+                    placeholderTextColor="#BDBDBD"
+                    // onFocus={() => {
+                    //   setIsShowKeyboard(true);
+                    // }}
+                  />
+                  <TouchableOpacity
+                    onPress={() => setIsHidePassword(!isHidePassword)}
+                    activeOpacity={0.7}
+                  >
+                    <Text style={styles.showPasswordText}>Show</Text>
+                  </TouchableOpacity>
+                </View>
+              </KeyboardAvoidingView>
+              <TouchableOpacity
+                onPress={keyboardHide}
+                style={styles.loginButton}
+                activeOpacity={0.7}
+              >
+                <Text style={styles.textBtn}>Login</Text>
+              </TouchableOpacity>
+              <TouchableOpacity activeOpacity={0.7}>
+                <Text style={styles.textRegister}>
+                  Don't have an account? Register
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableWithoutFeedback>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+  },
+  bgImage: {
+    flex: 1,
+    justifyContent: "flex-end",
+    resizeMode: "cover",
+    width: "100%",
+  },
+  formWrapper: {
+    height: "60.22%",
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    backgroundColor: "#fff",
+  },
+  text: {
+    textAlign: "center",
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 33,
+    fontFamily: "Roboto-Medium",
+    fontSize: 30,
+    fontStyle: "normal",
+    lineHeight: 35,
+    letterSpacing: 0.01,
+    color: "#212121",
+  },
+  input: {
+    height: 50,
+    paddingLeft: 16,
+    paddingRight: 16,
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    fontStyle: "normal",
+    lineHeight: 19,
+    color: "#212121",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    backgroundColor: "#F6F6F6",
+  },
+  showPasswordText: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    paddingTop: 16,
+    paddingRight: 16,
+    height: 50,
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    fontStyle: "normal",
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+  loginButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 51,
+    marginTop: 43,
+    marginBottom: 16,
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+  },
+  textBtn: {
+    fontFamily: "Roboto-Regular",
+    fontStyle: "normal",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#fff",
+  },
+  textRegister: {
+    textAlign: "center",
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    fontStyle: "normal",
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+});
---
+++ src/Screens/RegistrationScreen.jsx
@@ -0,0 +1,282 @@
+import { useState } from "react";
+
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+import bgImage from "../../assets/images/bg.jpg";
+import AddAvatarIcon from "../../assets/icons/addAvatarPlus.png";
+
+const initialState = {
+  login: "",
+  email: "",
+  password: "",
+};
+
+export default function RegistrationScreen() {
+  const [formData, setFormData] = useState(initialState);
+  const [isHidePassword, setIsHidePassword] = useState(true);
+  // const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { login, email, password } = formData;
+
+    if (
+      login.trim().length === 0 ||
+      email.trim().length === 0 ||
+      password.trim().length === 0
+    ) {
+      console.log("All fields are required!");
+      return;
+    }
+
+    const normalizedFormData = {
+      login: login.trim(),
+      email: email.trim().toLowerCase(),
+      password: password.trim(),
+    };
+
+    Keyboard.dismiss();
+    setFormData(initialState);
+    console.log(normalizedFormData, "Register data has been sent");
+  };
+
+  // const keyboardDidHideCallback = () => {
+  //   setIsShowKeyboard(false);
+  // };
+
+  const keyboardHide = () => {
+    //  setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
+
+  // useEffect(() => {
+  //   const keyboardDidHide = Keyboard.addListener(
+  //     "keyboardDidHide",
+  //     keyboardDidHideCallback
+  //   );
+
+  //   return () => {
+  //     keyboardDidHide?.remove();
+  //   };
+  // });
+
+  return (
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={keyboardHide}>
+        <ImageBackground source={bgImage} style={styles.bgImage}>
+          <TouchableWithoutFeedback onPress={keyboardHide}>
+            <View style={styles.formWrapper}>
+              <View style={styles.avatarWrapper}>
+                <View style={styles.avatarFrame}></View>
+                <Image source={AddAvatarIcon} style={styles.addAvatarIcon} />
+              </View>
+              <Text style={styles.text}>Registration</Text>
+              <View>
+                <KeyboardAvoidingView
+                  behavior={Platform.OS == "ios" ? "padding" : "height"}
+                >
+                  <View style={{ marginBottom: 16 }}>
+                    <TextInput
+                      style={styles.input}
+                      onChangeText={(value) =>
+                        setFormData((prevState) => ({
+                          ...prevState,
+                          login: value,
+                        }))
+                      }
+                      value={formData.login}
+                      placeholder="Login"
+                      keyboardType="default"
+                      placeholderTextColor="#BDBDBD"
+                      // onFocus={() => {
+                      //   setIsShowKeyboard(true);
+                      // }}
+                    />
+                  </View>
+                  <View style={{ marginBottom: 16 }}>
+                    <TextInput
+                      style={styles.input}
+                      onChangeText={(value) =>
+                        setFormData((prevState) => ({
+                          ...prevState,
+                          email: value,
+                        }))
+                      }
+                      value={formData.email}
+                      placeholder="Email"
+                      keyboardType="email-address"
+                      placeholderTextColor="#BDBDBD"
+                      // onFocus={() => {
+                      //   setIsShowKeyboard(true);
+                      // }}
+                    />
+                  </View>
+                  <View style={{ position: "relative", marginBottom: 43 }}>
+                    <TextInput
+                      style={{
+                        ...styles.input,
+                        paddingRight: 65,
+                      }}
+                      onChangeText={(value) =>
+                        setFormData((prevState) => ({
+                          ...prevState,
+                          password: value,
+                        }))
+                      }
+                      value={formData.password}
+                      placeholder="Password"
+                      secureTextEntry={isHidePassword ? true : false}
+                      keyboardType="default"
+                      placeholderTextColor="#BDBDBD"
+                    />
+                    <TouchableOpacity
+                      onPress={() => setIsHidePassword(!isHidePassword)}
+                      activeOpacity={0.7}
+                    >
+                      <Text style={styles.showPasswordText}>Show</Text>
+                    </TouchableOpacity>
+                  </View>
+                </KeyboardAvoidingView>
+              </View>
+
+              <TouchableOpacity
+                onPress={handleSubmit}
+                style={styles.registrationButton}
+                activeOpacity={0.7}
+              >
+                <Text style={styles.textBtn}>Register</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity activeOpacity={0.7}>
+                <Text style={styles.textLogin}>
+                  Already have an account? Login
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableWithoutFeedback>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+  },
+  bgImage: {
+    flex: 1,
+    justifyContent: "flex-end",
+    resizeMode: "cover",
+  },
+  formWrapper: {
+    height: "67.2%",
+    position: "relative",
+    paddingTop: 92,
+    paddingLeft: 16,
+    paddingRight: 16,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    backgroundColor: "#fff",
+  },
+  avatarWrapper: {
+    position: "absolute",
+    top: 0,
+    left: "50%",
+    transform: [{ translate: [-44, -60] }],
+  },
+  avatarFrame: {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    width: 120,
+    height: 120,
+    borderRadius: 16,
+    backgroundColor: "#F6F6F6",
+  },
+  addAvatarIcon: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    transform: [{ translate: [12, -12] }],
+    width: 25,
+    height: 25,
+  },
+  text: {
+    textAlign: "center",
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 33,
+    fontFamily: "Roboto-Medium",
+    fontSize: 30,
+    fontStyle: "normal",
+    lineHeight: 35,
+    letterSpacing: 0.01,
+    color: "#212121",
+  },
+  input: {
+    height: 50,
+    paddingLeft: 16,
+    paddingRight: 16,
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    fontStyle: "normal",
+    lineHeight: 19,
+    color: "#212121",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    backgroundColor: "#F6F6F6",
+  },
+  showPasswordText: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    paddingTop: 16,
+    paddingRight: 16,
+    height: 50,
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    fontStyle: "normal",
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+  registrationButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 51,
+    marginBottom: 16,
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+  },
+  textBtn: {
+    fontFamily: "Roboto-Regular",
+    fontStyle: "normal",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#fff",
+  },
+  textLogin: {
+    textAlign: "center",
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    fontStyle: "normal",
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+});
